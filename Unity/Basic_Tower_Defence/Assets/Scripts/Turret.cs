577befabcc176b595fb275c1557d41c2b94c44c5
Matti Soini <matti.soini.92@gmail.com>
2020-04-12 09:57:09 +0300

    Created tower defence and added enemies, waypoints, turrents, bullets and bullet effects

---
+++ Unity/Basic_Tower_Defence/Assets/Scripts/Turret.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;    
+    
+    [Header("Attributes")]
+    
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
+
+    public string enemyTag = "Enemy";
+    
+    public Transform PartToRotate;
+    public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float ShortestDistance = Mathf.Infinity;
+        GameObject NearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float DistanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (DistanceToEnemy < ShortestDistance)
+            {
+                ShortestDistance = DistanceToEnemy;
+                NearestEnemy = enemy;
+            }
+        }
+        if (NearestEnemy != null && ShortestDistance <= range)
+        {
+            target = NearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) 
+        {
+            return; 
+        }
+        //Target LockOn
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f )
+        {
+            shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+        if (bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Unity/Basic_Tower_Defence/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform EnemyPrefab;
+
+    public Transform SpawnPoint;
+    
+    public float TimeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text WaveCountdownText;
+
+    private int WaveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = TimeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        WaveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < WaveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        WaveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+
+}
